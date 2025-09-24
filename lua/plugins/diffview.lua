1c9465d24cfa35ef90324183cc29966c80277f76
Kingsley Yung <kingsley@kkoyung.dev>
2025-09-24 14:05:21 +0800

    Add diffview.nvim

---
+++ lua/plugins/diffview.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "sindrets/diffview.nvim",
+    keys = {
+      {
+        "<leader>gD",
+        "<cmd>DiffviewOpen<cr>",
+        desc = "DiffviewOpen",
+      },
+    },
+  },
+}
