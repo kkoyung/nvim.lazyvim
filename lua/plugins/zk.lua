b8d568d947028cf9a816146a5b322548ef9eca03
Kingsley Yung <kingsley@kkoyung.dev>
2025-09-24 14:04:11 +0800

    Add keymap for zk-nvim

--- lua/plugins/zk.lua
+++ lua/plugins/zk.lua
@@ -17,5 +17,35 @@ return {
         -- See Setup section below
       })
     end,
+    keys = {
+      {
+        "<leader>zf",
+        function()
+          require("zk.commands").get("ZkNotes")()
+        end,
+        desc = "zk notes",
+      },
+      {
+        "<leader>zn",
+        function()
+          require("zk.commands").get("ZkNew")()
+        end,
+        desc = "zk new",
+      },
+      {
+        "<leader>zl",
+        function()
+          require("zk.commands").get("ZkLinks")()
+        end,
+        desc = "zk links",
+      },
+      {
+        "<leader>zb",
+        function()
+          require("zk.commands").get("ZkBacklinks")()
+        end,
+        desc = "zk backlinks",
+      },
+    },
   },
 }
